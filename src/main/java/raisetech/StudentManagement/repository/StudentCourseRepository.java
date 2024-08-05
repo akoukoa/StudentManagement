dcca271860001b48c4b7fd08e49a621a575907f5
akoukoa <zomehura@gmail.com>
2024-08-05 13:21:26 +0900

    Read処理のServiceとController部分を実装

--- src/main/java/raisetech/StudentManagement/repository/StudentCourseRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentCourseRepository.java
@@ -1,12 +1,13 @@
-package raisetech.StudentManagement;
+package raisetech.StudentManagement.repository;
 
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import raisetech.StudentManagement.data.StudentCourses;
 
 @Mapper
 public interface StudentCourseRepository {
 
   @Select("SELECT * FROM students_courses")
-  List<StudentCourse> findAllCourse();
+  List<StudentCourses> findAllCourse();
 }
