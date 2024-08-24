ff78c4bb042e0498450b511cc6c22713ebdb0605
akoukoa <zomehura@gmail.com>
2024-08-24 09:52:22 +0900

    新規受講生情報を登録する処理を実装

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -54,7 +54,7 @@ public class StudentController {
     if(result.hasErrors()) {
       return "registerStudent";
     }
-    System.out.println(studentDetail.getStudent().getName() + "さんが新規受講生として登録されました。");
+    service.registerStudent(studentDetail);
     return "redirect:/studentList";
   }
 }
--- src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -1,7 +1,9 @@
 package raisetech.StudentManagement.repository;
 
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourses;
@@ -14,5 +16,10 @@ public interface StudentRepository {
 
   @Select("SELECT * FROM students_courses")
   List<StudentCourses> searchStudentsCourses();
+
+  @Insert("INSERT INTO students(name, kana_name, nickname, email, area, age, sex, remark, is_deleted)"
+      + " VALUE(#{name}, #{kanaName}, #{nickname}, #{email}, #{area}, #{age}, #{sex}, #{remark}, false)")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void registerStudent(Student student);
 }
 
